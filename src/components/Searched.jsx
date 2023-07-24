7f0651b8ef5cab81d4bfb6a8e8b04aa4861ebe79
koraykoksal <kksl.koray@gmail.com>
2023-07-24 15:21:11 +0300

    first commit

---
+++ src/components/Searched.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+import { IP_Context } from '../context/UserIPAddress'
+
+
+export const Searched = () => {
+
+
+  return (
+
+    <div className='contaciner'>
+      <div className='col-sm-4 col-md-4 m-auto text-center p-3'>
+       
+        <h5>Searched</h5>
+      </div>
+      
+    </div>
+  )
+}
